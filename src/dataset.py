4f0ae71d17bbd14b4a174e30515a730c2e700326
Santos Davi (CI/ION1) <Davi.Santos2@br.bosch.com>
2021-06-21 23:34:09 -0300

    Modified dataloader for working with yolo api

--- src/dataset.py
+++ src/dataset.py
@@ -3,11 +3,9 @@ import glob
 
 import pandas as pd
 import numpy as np
-import torch
-import PIL.Image as Image
-from torch.utils.data import Dataset
 
-class CNRExtDataset(Dataset):
+
+class CNRExtDataloader():
     def __init__(self,
                  targets_dir: str = '',
                  images_dir: str = '',
@@ -15,45 +13,63 @@ class CNRExtDataset(Dataset):
                  camera_ids: list = [],
                  image_size: list = [],
                  label_image_size: list = [],
+                 batch_size: int = 5,
+                 shuffle: bool = False
                  ):
 
         self.images_paths = []
+        self.targets = []
         self.targets_paths = [targets_dir + '/camera{}.csv'.format(i) for i in camera_ids]
-        self.targets_df = pd.DataFrame()
+        self.target_dict = {}
+
+        self.batch_size = batch_size
+        self.shuffle = shuffle
 
         self.image_size = image_size
         self.label_image_size = label_image_size
 
         self.__parse_bounding_boxes_csv()
         self.__load_images_path(images_dir, weather, camera_ids)
+        self.__image_target_pair()
 
     def __len__(self) -> int:
-        return len(self.images_paths)
+        return int(len(self.images_paths) / self.batch_size)
 
-    def __getitem__(self, idx: int):
-        image_path = self.images_paths[idx]
-        target = self.targets_df.loc[self.targets_df['camera'] == os.path.dirname(image_path)[-1:]].values
-
-        image = Image.open(image_path)
-        image = image.resize((1280, 640), Image.ANTIALIAS)
-        image = torch.from_numpy(np.array(image))
-        image = image.permute(2, 1, 0)
-        image = image/255.0
-
-        target = torch.from_numpy(target)
-
-        return image, target
+    def __iter__(self):
+        if self.shuffle:
+            indices = np.random.permutation(len(self.images_paths))
+        else:
+            indices = [idx for idx in range(0, len(self.images_paths))]
+        batch = []
+        for i in indices:
+            batch.append(i)
+            if len(batch) == self.batch_size:
+                yield self.images_paths[batch].tolist(), self.targets[batch]
+                batch = []
 
     def __parse_bounding_boxes_csv(self):
         for f in self.targets_paths:
             targets_df = pd.read_csv(f)
             targets_df[['X', 'W']] = targets_df[['X', 'W']] * self.image_size[0] / self.label_image_size[0]
             targets_df[['Y', 'H']] = targets_df[['Y', 'H']] * self.image_size[1] / self.label_image_size[1]
-            targets_df['camera'] = int(os.path.splitext(f)[0][-1])
 
-            self.targets_df = self.targets_df.append(targets_df)
+            targets_df['X1'] = targets_df['X']
+            targets_df['Y1'] = targets_df['Y']
+            targets_df['X2'] = targets_df['X'] + targets_df['W']
+            targets_df['Y2'] = targets_df['Y'] + targets_df['H']
+
+            targets_df[['camera']] = int(os.path.splitext(f)[0][-1])
+
+            self.target_dict[int(os.path.splitext(f)[0][-1])] = \
+                targets_df[['X1', 'Y1', 'X2', 'Y2', 'SlotId', 'camera']].values
 
     def __load_images_path(self, images_dir, weather, camera_ids):
         for w in weather:
             for i in camera_ids:
                 self.images_paths.extend(glob.glob(images_dir + "/" + w + '**/*/*{}/*jpg'.format(i), recursive=True))
+        self.images_paths = np.array(self.images_paths)
+
+    def __image_target_pair(self):
+        for path in self.images_paths:
+            self.targets.append(self.target_dict[int(os.path.basename(os.path.dirname(path))[-1:])])
+        self.targets = np.array(self.targets)
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import json
 
 import torch
 
-from dataset import CNRExtDataset
+from dataset import CNRExtDataloader
 from model import ParkingSpotClassifier
 import src.utils as utils
 
@@ -21,15 +21,13 @@ def main(config_path):
     with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
 
-    dataset = CNRExtDataset(targets_dir=config['dataset']['root_path'],
+    dataloader = CNRExtDataloader(targets_dir=config['dataset']['root_path'],
                             images_dir=config['dataset']['root_path']+config['dataset']['images_dir'],
                             weather=config['dataset']['params']['weather'],
                             camera_ids=config['dataset']['params']['camera_ids'],
                             label_image_size=config['dataset']['params']['label_image_size'],
-                            image_size=config['dataset']['params']['image_size'])
-
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=config['batch_size'], num_workers=config['num_workers'],
-                                 shuffle=False)
+                            image_size=config['dataset']['params']['image_size'],
+                            batch_size=config['batch_size'], shuffle=False)
 
     model = torch.hub.load(repo_or_dir=config['model']['repository'], model=config['model']['name'],
                            pretrained=config['model']['pretrained'])
@@ -37,9 +35,9 @@ def main(config_path):
 
     model.eval()
     for images, targets in dataloader:
-
-        outputs = model(images)
-        print(outputs)
+        images_paths = images[:]
+        results = model(images)
+        utils.display_detected_and_target(images_paths, results.xyxy, targets)
 
 if __name__ == '__main__':
     _args = arg_parser.parse_args()
