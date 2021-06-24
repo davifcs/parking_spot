7567b413da9afb078b58bf9913af4143180c6104
Santos Davi (CI/ION1) <Davi.Santos2@br.bosch.com>
2021-06-23 23:29:32 -0300

    Working version for detecting and evaluating vehicles in a slot

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 
 import torch
+import numpy as np
 
 from dataset import CNRExtDataloader
 import src.utils as utils
@@ -20,25 +21,45 @@ def main(config_path):
     with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
 
-    dataloader = CNRExtDataloader(targets_dir=config['dataset']['root_path'],
-                            images_dir=config['dataset']['root_path']+config['dataset']['images_dir'],
-                            weather=config['dataset']['params']['weather'],
-                            camera_ids=config['dataset']['params']['camera_ids'],
-                            label_image_size=config['dataset']['params']['label_image_size'],
-                            image_size=config['dataset']['params']['image_size'],
-                            batch_size=config['batch_size'], shuffle=False)
+    if config['mode'] == "detection":
+        dataloader = CNRExtDataloader(targets_dir=config['dataset']['root_path'],
+                                images_dir=config['dataset']['root_path']+config['dataset']['images_dir'],
+                                weather=config['dataset']['params']['weather'],
+                                camera_ids=config['dataset']['params']['camera_ids'],
+                                label_image_size=config['dataset']['params']['label_image_size'],
+                                image_size=config['dataset']['params']['image_size'],
+                                batch_size=config['batch_size'], shuffle=False)
 
-    model = torch.hub.load(repo_or_dir=config['model']['repository'], model=config['model']['name'],
-                           pretrained=config['model']['pretrained'])
-    model.classes = config['model']['classes']
+        model = torch.hub.load(repo_or_dir=config['model']['repository'], model=config['model']['name'],
+                               pretrained=config['model']['pretrained'])
+        model.classes = config['model']['classes']
 
-    model.eval()
-    for images, targets in dataloader:
-        images_paths = images[:]
-        results = model(images)
-        n_detections = utils.detection(results.xyxy, targets)
-        utils.display_targets_and_detections(images_paths, results.xyxy, targets, n_detections)
+        n_detections_camera = np.empty((0, 3))
+        detections_xy_camera = np.empty((0, 3))
 
+        model.eval()
+        for images, targets in dataloader:
+            images_paths = images[:]
+            results = model(images)
+            camera_ids = np.array([i[:, 5][0] for i in targets])
+            if config['task'] == "evaluate":
+                n_detections, n_targets = utils.detections(results.xyxy, targets)
+                n_detections_camera = np.vstack((n_detections_camera, np.hstack((n_detections.reshape(-1, 1),
+                                                                                 n_targets.reshape(-1, 1),
+                                                                                 camera_ids.reshape(-1, 1)))))
+                utils.display_targets_and_detections(images_paths, results.xyxy, targets, n_detections)
+
+        if config['task'] == "evaluate":
+            detection_rate = {}
+            for camera in np.unique(n_detections_camera[:, 2]):
+                detection_rate[camera] = {}
+                detection_rate[camera]['n_detections'] = max(n_detections_camera[n_detections_camera[:, 2] == camera][:, 0])
+                detection_rate[camera]['n_targets'] = n_detections_camera[n_detections_camera[:, 2] == camera][0][1]
+                detection_rate[camera]['ratio'] = detection_rate[camera]['n_detections']/detection_rate[camera]['n_targets']
+
+            with open("./results/detection_rate.txt", "a") as file:
+                file.write(str(detection_rate) + "\n")
+            print(detection_rate)
 
 if __name__ == '__main__':
     _args = arg_parser.parse_args()
--- src/utils.py
+++ src/utils.py
@@ -6,31 +6,38 @@ from sklearn.cluster import KMeans
 
 
 # TODO Needs refactor
-def k_means_clusters(output, n_clusters):
+def k_means_clusters(detected, n_clusters):
     detected_xy = []
-    for o in output:
-        detected_xy.extend(o[:, :2].cpu().tolist())
+    for d in detected:
+        detected_xy.extend(d[:, :2].cpu().tolist())
     kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(detected_xy)
     return kmeans.cluster_centers_
 
 
-def detection(detected, target):
-    rate = []
+def detections(detected, target):
+    n_detections = []
+    n_targets = []
     for d, t in zip(detected, target):
         t_ = np.array(t)
         t_xyxy = t_[:, :4]
-        d_xy = []
-        center = (d[:, :2] + d[:, 2:4])//2
-        d_xy.extend(center.cpu().tolist())
-        d_xy = np.array(d_xy)
-        rate.append(sum(((d_xy >= t_xyxy[:, None, :2]) & (d_xy <= t_xyxy[:, None, 2:])).all(2).any(1)))
-    return rate
+        if len(d) > 0:
+            d_xy = []
+            center = (d[:, :2] + d[:, 2:4])//2
+            d_xy.extend(center.cpu().tolist())
+            d_xy = np.array(d_xy)
+            n_detections.append(sum(((d_xy >= t_xyxy[:, None, :2]) & (d_xy <= t_xyxy[:, None, 2:])).all(2).any(1)))
+            n_targets.append(len(t_xyxy))
+        else:
+            n_detections.append(0)
+            n_targets.append(len(t_xyxy))
+    return np.array(n_detections), np.array(n_targets)
 
 
 def display_targets_and_detections(image_path, detected, target, n_detections):
     for p, d, t, n in zip(image_path, detected, target, n_detections):
         image = Image.open(p).convert('RGB')
         draw = ImageDraw.Draw(image)
+        output_path = os.path.abspath('.') + '/results/' + os.path.basename(os.path.split(p)[0]) + '/' + os.path.basename(p)
         for d_bbox in d:
             draw.rectangle(d_bbox[:4].cpu().detach().numpy(), outline='red', width=2)
             draw.line(d_bbox.cpu().detach().numpy()[:4], fill='red', width=2)
@@ -40,4 +47,4 @@ def display_targets_and_detections(image_path, detected, target, n_detections):
             draw.rectangle(t_bbox[:4], outline='green', width=2)
         font = ImageFont.truetype("arial.ttf", 32)
         draw.text((0, 0), str(n) + '/' + str(len(t)), (255, 0, 0), font=font)
-        image.save(os.path.abspath('.') + '/results/camera' + str(int(t_bbox[5])) + '/' + os.path.basename(p))
+        image.save(output_path)
